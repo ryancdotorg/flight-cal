d64346ed068b21a0b1e724108f7dac60b7d1a487
Ryan Castellucci <code@ryanc.org>
2024-05-19 15:30:21 +0100

    allow date to be omitted for arrival

--- cal/cal.go
+++ cal/cal.go
@@ -11,9 +11,32 @@ import (
     "github.com/blalor/flight-cal/tz_lookup"
 )
 
-func CreateFlightCal(prefix string, flight, departAirport string, departTimeStr string, arriveAirport string, arriveTimeStr string) (*ics.Calendar, error) {
-    const timeFmt = "2006-01-02T15:04"
+const timeFmtLong = "2006-01-02T15:04"
+const timeFmtTime = "15:04"
+const timeFmtDate = "2006-01-02"
 
+func changeDay(t time.Time, dir time.Duration) time.Time {
+    loc := t.Location()
+    day := t.Format(timeFmtDate)
+    tmp := t.Add(time.Hour * dir)
+    for tmp.Format(timeFmtDate) == day {
+        tmp = tmp.Add(time.Hour * dir)
+    }
+
+    timeStr := fmt.Sprintf("%sT%s", tmp.Format(timeFmtDate), t.Format(timeFmtTime))
+    t2, _ := time.ParseInLocation(timeFmtLong, timeStr, loc)
+    return t2
+}
+
+func prevDay(t time.Time) time.Time {
+    return changeDay(t, -1)
+}
+
+func nextDay(t time.Time) time.Time {
+    return changeDay(t, 1)
+}
+
+func CreateFlightCal(prefix string, flight, departAirport string, departTimeStr string, arriveAirport string, arriveTimeStr string) (*ics.Calendar, error) {
     departApt, err := ap_info.LookupByIata(departAirport)
     if err != nil {
         return nil, err
@@ -28,7 +51,7 @@ func CreateFlightCal(prefix string, flight, departAirport string, departTimeStr 
         return nil, err
     }
 
-    departTime, err := time.ParseInLocation(timeFmt, departTimeStr, departTz)
+    departTime, err := time.ParseInLocation(timeFmtLong, departTimeStr, departTz)
     if err != nil {
         return nil, err
     }
@@ -47,9 +70,27 @@ func CreateFlightCal(prefix string, flight, departAirport string, departTimeStr 
         return nil, err
     }
 
-    arriveTime, err := time.ParseInLocation(timeFmt, arriveTimeStr, arriveTz)
+    arriveTime, err := time.ParseInLocation(timeFmtLong, arriveTimeStr, arriveTz)
     if err != nil {
-        return nil, err
+        departDayStr := departTime.Format(timeFmtDate)
+        maybeTimeStr := fmt.Sprintf("%sT%s", departDayStr, arriveTimeStr)
+        arriveTime, err = time.ParseInLocation(timeFmtLong, maybeTimeStr, arriveTz)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    duration := arriveTime.Sub(departTime).Hours()
+
+    if duration < -22 {
+        arriveTime = nextDay(nextDay(arriveTime))
+        duration = arriveTime.Sub(departTime).Hours()
+    } else if duration < 0 {
+        arriveTime = nextDay(arriveTime)
+        duration = arriveTime.Sub(departTime).Hours()
+    } else if duration > 22 {
+        arriveTime = prevDay(arriveTime)
+        duration = arriveTime.Sub(departTime).Hours()
     }
 
     c := ics.NewCalendar()
